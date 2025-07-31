0448176aa3a4b62a6674568a219f8b0ff43acb70
thoitietvn <truyenhinhcap89@gmail.com>
2025-07-31 16:57:08 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+tram anh
+
+# Hướng dẫn sơ sài :
+* pkg update && pkg upgrade -y
+* pkg install root-repo -y
+* pip install pycryptodome
+* pkg install git tsu python wpa-supplicant pixiewps iw openssl -y
+* git clone https://github.com/minhdongdasat1987/fixwifi.git
+* cd fixwifi
+* chmod +x mlink.py
+* sudo python mlink.py -i wlan0 -K
---
+++ mlink.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import subprocess
+import os
+import tempfile
+import shutil
+import re
+import codecs
+import socket
+import pathlib
+import time
+from datetime import datetime
+import collections
+import statistics
+import csv
+from pathlib import Path
+from typing import Dict
+
+
+class NetworkAddress:
+    def __init__(self, mac):
+        if isinstance(mac, int):
+            self._int_repr = mac
+            self._str_repr = self._int2mac(mac)
+        elif isinstance(mac, str):
+            self._str_repr = mac.replace('-', ':').replace('.', ':').upper()
+            self._int_repr = self._mac2int(mac)
+        else:
+            raise ValueError('MAC address must be string or integer')
+
+    @property
+    def string(self):
+        return self._str_repr
+
+    @string.setter
+    def string(self, value):
+        self._str_repr = value
+        self._int_repr = self._mac2int(value)
+
+    @property
+    def integer(self):
+        return self._int_repr
+
+    @integer.setter
+    def integer(self, value):
+        self._int_repr = value
+        self._str_repr = self._int2mac(value)
+
+    def __int__(self):
+        return self.integer
+
+    def __str__(self):
+        return self.string
+
+    def __iadd__(self, other):
+        self.integer += other
+
+    def __isub__(self, other):
+        self.integer -= other
+
+    def __eq__(self, other):
+        return self.integer == other.integer
+
+    def __ne__(self, other):
+        return self.integer != other.integer
+
+    def __lt__(self, other):
+        return self.integer < other.integer
+
+    def __gt__(self, other):
+        return self.integer > other.integer
+
+    @staticmethod
+    def _mac2int(mac):
+        return int(mac.replace(':', ''), 16)
+
+    @staticmethod
+    def _int2mac(mac):
+        mac = hex(mac).split('x')[-1].upper()
+        mac = mac.zfill(12)
+        mac = ':'.join(mac[i:i+2] for i in range(0, 12, 2))
+        return mac
+
+    def __repr__(self):
+        return 'NetworkAddress(string={}, integer={})'.format(
+            self._str_repr, self._int_repr)
+
+# Ví dụ minh hoạ xuất kết quả có dòng phân cách
+
+def display_result(wifi_name, password):
+    print(f"WiFi: {wifi_name}")
+    print(f"Password: {password}")
+    print("[94m" + "=" * 50 + "[0m")  # Dòng kẻ xanh dương
+
+if __name__ == "__main__":
+    results = [
+        ("MyWiFi1", "12345678"),
+        ("MyWiFi2", "abcdefgh"),
+    ]
+    for wifi, pwd in results:
+        display_result(wifi, pwd)
